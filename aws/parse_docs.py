d07d3a35655336f63f011dfc1091242165969d16
DesiPilla <dmpilla@udel.edu>
2020-12-16 20:09:35 -0500

    Update AWS serverless Lambda functions

--- aws/parse_docs.py
+++ aws/parse_docs.py
@@ -2,82 +2,173 @@ import os, sys
 sys.path.insert(1, '/mnt/efs/')
 print("Mounted.")
 
+# import pandas as pd
+# pd.read_excel("https://github.com/DesiPilla/college-emails/blob/master/CollegeEmails.xlsx?raw=true")
+# print("Connected to internet.")
+
+# Start function timer
 import time
-start_time = time.time()
 
 import io
-import re
 import json
 import boto3
-import pickle
-import stanza
+import psutil
 import logging
 import pandas as pd
-from pymongo import MongoClient
 print("Imported.")
 
-# Load AWS Credentials
 aws_id, aws_secret_key = pd.read_csv('DesiPilla_accessKeys.csv').values[0]
 print("Keys loaded.")
+
 bucket = os.environ["bucket"]
 queue_name = os.environ["queueName"]
 queue_url = os.environ["queueUrl"]
 
-# Configure AWS clients
-s3 = boto3.client('s3', aws_access_key_id=aws_id, aws_secret_access_key=aws_secret_key)
-s3_resource = boto3.resource('s3', aws_access_key_id=aws_id, aws_secret_access_key=aws_secret_key)
-sqs = boto3.client('sqs')
+s3 = boto3.client('s3')
+s3_resource = boto3.resource('s3')
+# s3 = boto3.client('s3', aws_access_key_id=aws_id, aws_secret_access_key=aws_secret_key)
+# s3_resource = boto3.resource('s3', aws_access_key_id=aws_id, aws_secret_access_key=aws_secret_key)
+sqs = boto3.client('sqs', aws_access_key_id=aws_id, aws_secret_access_key=aws_secret_key)
 print("s3 and sqs connected.")
 
-# Load Stanford CoreNLP pipeline
-obj = s3.get_object(Bucket=bucket, Key='nlp')
-data = obj['Body'].read()
-nlp = pickle.loads(data)
-print("NLP loaded.")
 
-def prepare_doc(doc):
-    doc["source"] = doc["website"]
-    doc["context"] = doc['text']
+# Load English neural pipeline
+import stanza
+nlp = stanza.Pipeline('en', '/mnt/efs', processors="tokenize, pos, lemma, ner, depparse") 
+print("CoreNLP loaded.")
 
-    strip = [k for k in doc.keys() if "." in k ] + ["website", "text"]
-    for k in strip:
-        del doc[k]
 
-    doc['stanford'] = 0
-    # doc['_id'] = doc['id']
-    return doc
+
+def parse_doc(doc):
+    
+    # Extract document information
+    doc_id = doc["id"]
+    text = doc["text"]
+    title = doc["title"]
+    url = doc["url"]
+    decade = doc["decade"]
+    date = doc["date"]
+    president = doc["website"]
+
+    # Only keep the first 1,600 words
+    if doc["num_words"] > 1600:
+        text = ' '.join(text.split(' ')[:1600])
+
+    try:
+        # Parse document text
+        parsed = nlp(text)
+        
+        # Isolate sentence results and dependency strings
+        parse_results = [sentence.to_dict() for sentence in parsed.sentences]
+        dep_strs = [sentence.dependencies_string() for sentence in parsed.sentences]
+    
+    
+        # Construct message
+        message = {
+            "id": doc_id,
+            "text": text,
+            "title": title,
+            "url": url,
+            "date": date,
+            "website": president,
+            
+            "parse_results": json.dumps(parse_results),
+            "dependency_strings": json.dumps(dep_strs),
+        }
+
+        # Create S3 key
+        key = "parsed/{}/{}/{}.json".format(president, decade, doc_id)
+        
+        return message, key
+        
+    # Handle any exceptions thrown during processing
+    except:
+        return 'error', None
+
+
 
 
 def handler(event, context):
-    print("Enter handler.")
     start_time = time.time()
-    
-    mongo = MongoClient('mongodb://52.71.228.156:27017/')
-    db = mongo.FRUS
-    collection = db.ParsedDocs
-    print("Mongo connected.")
+    print("Enter handler.")
 
-    count = 0
+    # Initialize progress counters
+    successes = 0
+    errors = 0
+
+    # Keep parsing documents one at a time for 1 minutes
+    counter = 0
     while time.time() - start_time < 600:
-        print("Parsing doc {:,}...".format(count), end="")
-        resp = sqs.receive_message(QueueUrl=queue_url, AttributeNames=['All'])
-        doc = json.loads(resp["Messages"][0]["Body"])
-        msg_receipt = resp["Messages"][0]["ReceiptHandle"]
-
-        text = doc["text"][:2000]
-        print("({:,})...".format(len(text)), end="")
-        try:
-            parsed = nlp(text).to_dict()[0]
-            doc["parsed"] = json.dumps(parsed)
-        except:
-            doc["parsed"] = "error"
-
-        to_mongo = prepare_doc(doc)
-        collection.insert_one(to_mongo)
-        del_resp = sqs.delete_message(QueueUrl=queue_url, ReceiptHandle=msg_receipt)
-        print("Done")
-        count += 1
+        counter += 1
         
+        try:
+            # Fetch document from SQS
+            resp = sqs.receive_message(QueueUrl=queue_url, AttributeNames=['All'])
+            doc = json.loads(resp["Messages"][0]["Body"])
+            msg_receipt = resp["Messages"][0]["ReceiptHandle"]
+            print("Parsing doc {} ({} words, id = {})".format(counter, doc["num_words"], doc["url"]))
+        except:
+            print("Empty message recevied.")
+            continue
+        
+        # Parse document and get JSON output
+        doc_start = time.time()
+        message, key = parse_doc(doc)
+        print("\tParsed in {:.1f} seconds.".format(time.time() - doc_start))
+    
+        # If no error was thrown
+        if key:
+            # Save output to s3
+            s3_resp = s3.put_object(
+                Body=json.dumps(message),
+                Bucket=bucket,
+                Key=key
+                )
+
+            # Check that results were saved to s3 successfully
+            if s3_resp["ResponseMetadata"]["HTTPStatusCode"] != 200:
+                print(s3_resp)
+                errors += 1
+                del message
+                del resp
+                del doc
+                del msg_receipt
+                continue
+
+            # Delete document from SQS
+            del_resp = sqs.delete_message(
+                QueueUrl = queue_url,
+                ReceiptHandle = msg_receipt
+            )
             
-    print("{:,} documents parsed and sent to Mongo.".format(count))
-    return {"message":"{:,} documents parsed and sent to Mongo.".format(count)}
+            # Check that document was deleted from SQS successfully
+            if del_resp["ResponseMetadata"]["HTTPStatusCode"] != 200:
+                print(del_resp)
+                errors += 1
+                del message
+                del resp
+                del doc
+                del msg_receipt
+                continue
+            
+            successes += 1
+        else: 
+            print(key)
+            print(message)
+            errors += 1
+            del message
+            del resp
+            del doc
+            del msg_receipt
+            continue
+        
+        print("{:.2f} MB available".format(psutil.virtual_memory().available * 1e-6))
+        # Delete message from memory to cut costs
+        del message
+        del resp
+        del doc
+        del msg_receipt
+
+    return {
+        "message": "{:,} documents were processed.\n{:,} were parsed successfuly and {:,} threw errors.".format(successes + errors, successes, errors)
+    }
--- aws/test.py
+++ aws/test.py
@@ -2,15 +2,20 @@ import os, sys
 sys.path.insert(1, '/mnt/efs/')
 print("Mounted.")
 
-import boto3
+import psutil
+print(psutil.virtual_memory())
+
+import io
 import json
-import pickle
+import time
+import boto3
+import logging
 import pandas as pd
-from pymongo import MongoClient
 print("Imported.")
 
 aws_id, aws_secret_key = pd.read_csv('DesiPilla_accessKeys.csv').values[0]
 print("Keys loaded.")
+
 bucket = os.environ["bucket"]
 queue_name = os.environ["queueName"]
 queue_url = os.environ["queueUrl"]
@@ -22,24 +27,127 @@ s3_resource = boto3.resource('s3', aws_access_key_id=aws_id, aws_secret_access_k
 sqs = boto3.client('sqs')
 print("s3 and sqs connected.")
 
-mongo = MongoClient('mongodb://52.71.228.156:27017/')
-print("Mongo connected.")
+print(psutil.virtual_memory())
+
+# from stanfordcorenlp import StanfordCoreNLP
+# nlp = StanfordCoreNLP("/mnt/efs/stanford-corenlp-full-2018-10-05", timeout=600, lang="en", memory="8g", quiet=False, logging_level=logging.INFO)
+# print(nlp.port)
+# print(nlp.url)
+
+print(psutil.virtual_memory())
+
+# import stanza
+# from stanza.server import CoreNLPClient
+# classpath = '/mnt/efs/stanford-corenlp-full-2018-10-05'
+# nlp = CoreNLPClient(be_quite=False, 
+#                       classpath=classpath, 
+#                       annotators=['tokenize'],# 'mwt', 'pos', 'lemma', 'depparse'], 
+#                       memory='4G', 
+#                       endpoint='http://localhost:9001')
+# nlp.start()
+
+# ann = nlp.annotate(text)
 
 import stanza
-# nlp = stanza.Pipeline('en', '/mnt/efs/')
-nlp = s3.get_object(Bucket=bucket, Key='dataframes/test.csv')
-print("NLP loaded.")
+# Load English neural pipeline
+nlp = stanza.Pipeline('en', '/mnt/efs', processors="tokenize, pos, lemma, ner, depparse") 
+
+
+
+print("CoreNLP loaded.")
+# print("Initializing CoreNLP with 'I' parse...")
+# start_time = time.time()
+# text = "Barack Obama was the 45th President of the United States. The clothes in the dressing room are gorgeous. Can I have one?"
+# prop={'annotators': 'tokenize,ssplit,pos,','pipelineLanguage':'en','outputFormat':'xml'}
+# ann = nlp.annotate(text, properties=prop)
+# ann = nlp(text)
+# print(ann)
+
+# print("Done. (initialized in {:.1} seconds".format(time.time() - start_time))
+
+
+def parse_doc(doc):
+    
+    # Extract document information
+    doc_id = doc.id
+    text = doc.text
+    title = doc.title
+    url = doc.url
+    date = doc.date
+    president = doc.website
+
+    # Only keep the first 3,000 words
+    if doc.num_words > 3000:
+        text = ' '.join(text.split(' ')[:3000])
+
+    try:
+        # Parse document text
+        parsed = nlp(text)
+        
+        # Isolate sentence results and dependency strings
+        parse_results = [sentence.to_dict() for sentence in parsed.sentences]
+        dep_strs = [sentence.dependencies_string() for sentence in parsed.sentences]
+    
+    
+        # Construct message
+        message = {
+            "id": doc_id,
+            "text": text,
+            "title": title,
+            "url": url,
+            "date": date,
+            "website": president,
+            
+            "parse_results": json.dumps(parse_results),
+            "dependency_strings": json.dumps(dep_strs),
+        }
+        
+        return message
+        
+    # Handle any exceptions thrown during processing
+    except:
+        return 'error'
 
 
 def handler(event, context):
     print("Enter handler.")
+
+    # Initialize progress counters
+    successes = 0
+    errors = 0
+
+    # resp = sqs.receive_message(QueueUrl = queue_url, AttributeNames=['All'])
+    # doc = json.loads(resp["Messages"][0]["Body"])
+    # msg_receipt = resp["Messages"][0]["ReceiptHandle"]
+
+
+    # Get document
+    key = "parsed/test.csv"
+    data = s3_resource.Object(bucket, key).get()['Body'].read()
+    df = pd.read_csv(io.BytesIO(data)).iloc[:, 1:]
     
-    resp = sqs.receive_message(QueueUrl = queue_url, AttributeNames=['All'])
-    doc = json.loads(resp["Messages"][0]["Body"])
-    msg_receipt = resp["Messages"][0]["ReceiptHandle"]
+    for i in range(10):
+        # Load document
+        doc = df.iloc[i]
+        print("Parsing doc {} (id = {})".format(i, doc.url))
+        
+        # Parse document and get JSON output
+        print("Parsing example document...")
+        start_time = time.time()
+        message = parse_doc(doc)
+        print("Done. (parsed in {:.1f} seconds)".format(time.time() - start_time))
+    
+        if message != 'error': successes += 1
+        else: errors += 1
+    
 
+    # # Save output
+    # s3.put_object(
+    #         Body=str(json.dumps(xml)),
+    #         Bucket=bucket,
+    #         Key="parsed/test_output.xml"
+    # )
 
-
-    return {"message":dbs,
-            "ip": str(ip)
+    return {
+        "message": "{:,} documents were processed.\n{:,} were parsed successfuly and {:,} threw errors.".format(successes + errors, successes, errors)
     }
